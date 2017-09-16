f4492acb66110532eeff5f96ee71892132f2538c
Hopegoing <zlwork2014@gmail.com>
2017-09-16 19:40:01 +0800

    修改用户头像和用户名展现形式

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# 修改了自定义标签要重启服务器
+
 from django import template
 
 register = template.Library()
@@ -9,3 +11,15 @@ def get_email_secret(email):
     e_right = email.split('@')[-1]
     e_left = email.split('@')[0]
     return e_left[:2]+'...'+'@'+e_right
+
+@register.filter
+def get_show_name(user):
+    '''返回用户的展示名，优先选择昵称'''
+    if user.nickname:
+        return user.nickname
+    return user.username
+
+@register.inclusion_tag('blog/tags/user-avatar.html')
+def get_show_avatar(user):
+    '''返回一个用户的展示头像<img>标签，优先用网络头像'''
+    return {'user':user}
