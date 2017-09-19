38f36055c35e4dadb8195e822c3cbd94cc61baf1
Hopegoing <zlwork2014@gmail.com>
2017-09-20 00:46:09 +0800

    添加回到顶部

--- blog/static/blog/mystyle/js/base-js.js
+++ blog/static/blog/mystyle/js/base-js.js
@@ -2,3 +2,15 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+//回到顶部
+$(window).scroll(function(){
+    $('#to-top').hide();
+    if ($(window).scrollTop()>=500){
+        $('#to-top').show();
+    };
+});
+$("#to-top").click(function () {
+        var speed=400;//滑动的速度
+        $('body,html').animate({ scrollTop: 0 }, speed);
+        return false;
+ });
