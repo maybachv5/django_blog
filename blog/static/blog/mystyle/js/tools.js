8880c2d22bdad923962c5ef3a5ac1e21860185f8
Hopegoing <zlwork2014@gmail.com>
2017-12-04 17:27:32 +0800

    debug01

--- blog/static/blog/mystyle/js/tools.js
+++ blog/static/blog/mystyle/js/tools.js
@@ -12,39 +12,31 @@ function wordsearch_func(URL){
             var tmret = ret.tm_results;
             var jdret = ret.jd_results;
             var vipret = ret.vip_results;
-            if (tbret){
-                for (var i=0;i<tbret.length;i++){
-                tbhtml += '<tr><th scope="row">' + (i+1) + '</th>' + '<td>' + tbret[i][0] +
-                '</td>' + '<td>' + tbret[i][1] + '</td></tr>'
-                };
-                $("#tb-results").html(tbhtml);
+            
+            for (var i=0;i<tbret.length;i++){
+            tbhtml += '<tr><th scope="row">' + (i+1) + '</th>' + '<td>' + tbret[i][0] +
+            '</td>' + '<td>' + tbret[i][1] + '</td></tr>'
             };
+            $("#tb-results").html(tbhtml);
 
-            if (tmret){
-                for (var j=0;j<tmret.length;j++){
-                tmhtml += '<tr><th scope="row">' + (j+1) + '</th>' + '<td>' + tmret[j][0] + '</td>' +
-                '<td>' + tmret[j][1] + '</td>' + '<td>' + tmret[j][2] + '</td>' +
-                '<td>' + tmret[j][3] + '</td></tr>'
-                };
-                $("#tm-results").html(tmhtml);
+            for (var j=0;j<tmret.length;j++){
+            tmhtml += '<tr><th scope="row">' + (j+1) + '</th>' + '<td>' + tmret[j][0] + '</td>' +
+            '<td>' + tmret[j][1] + '</td>' + '<td>' + tmret[j][2] + '</td>' +
+            '<td>' + tmret[j][3] + '</td></tr>'
             };
+            $("#tm-results").html(tmhtml);
 
-            if (jdret){
-                for (var k=0;k<jdret.length;k++){
-                jdhtml += '<tr><th scope="row">' + (k+1) + '</th>' + '<td>' + jdret[k].key +
-                '</td>' + '<td>' + jdret[k].qre + '</td></tr>'
-                };
-                $("#jd-results").html(jdhtml);
+            for (var k=0;k<jdret.length;k++){
+            jdhtml += '<tr><th scope="row">' + (k+1) + '</th>' + '<td>' + jdret[k].key +
+            '</td>' + '<td>' + jdret[k].qre + '</td></tr>'
             };
+            $("#jd-results").html(jdhtml);
 
-
-            if (vipret){
-                for (var v=0;v<vipret.length;v++){
-                viphtml += '<tr><th scope="row">' + (v+1) + '</th>' + '<td>' + vipret[v].word +
-                '</td>' + '<td>' + vipret[v].goodscount + '</td>' + '<td>' + vipret[v].words + '</td></tr>'
-                };
-                $("#vip-results").html(viphtml);
+            for (var v=0;v<vipret.length;v++){
+            viphtml += '<tr><th scope="row">' + (v+1) + '</th>' + '<td>' + vipret[v].word +
+            '</td>' + '<td>' + vipret[v].goodscount + '</td>' + '<td>' + vipret[v].words + '</td></tr>'
             };
+            $("#vip-results").html(viphtml);
         });
     };
     if (word.length<1){
